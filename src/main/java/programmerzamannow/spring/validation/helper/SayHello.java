400672fe8ba82e5f309b7ec345b3044f6d67d832
Gilang Chandra Maulana <gilangcm17@gmail.com>
2023-12-02 16:50:26 +0700

    Belajar Spring Validation

---
+++ src/main/java/programmerzamannow/spring/validation/helper/SayHello.java
@@ -0,0 +1,13 @@
+package programmerzamannow.spring.validation.helper;
+
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.stereotype.Component;
+import org.springframework.validation.annotation.Validated;
+
+@Component
+public class SayHello implements ISayHello{
+
+    public String sayHello(@NotBlank String name){
+        return "hello " + name;
+    }
+}
---
+++ src/test/java/programmerzamannow/spring/validation/SayHelloTest.java
@@ -0,0 +1,29 @@
+package programmerzamannow.spring.validation;
+
+import jakarta.validation.ConstraintViolationException;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import programmerzamannow.spring.validation.helper.SayHello;
+
+@SpringBootTest
+public class SayHelloTest {
+
+    @Autowired
+    private SayHello sayHello;
+
+    @Test
+    void testSuccess() {
+        String message = sayHello.sayHello("gils");
+
+        Assertions.assertEquals("hello gils", message);
+    }
+
+    @Test
+    void testError() {
+        Assertions.assertThrows(ConstraintViolationException.class, () -> {
+            sayHello.sayHello("");
+        });
+    }
+}
